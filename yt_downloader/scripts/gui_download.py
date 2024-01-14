863993d1f1c874b384ecd665a237916988f10f6f
alllexxx1 <alekkorpachyov@gmail.com>
2024-01-14 17:59:36 +0300

    Add an opportunity to change by-default theme in gui-download script

--- yt_downloader/scripts/gui_download.py
+++ yt_downloader/scripts/gui_download.py
@@ -3,10 +3,16 @@
 
 from yt_downloader.gui_downloader import gui_download
 from yt_downloader.downloader import download
+from sys import argv
 
 
 def main():
-    gui_download(download)
+    try:
+        if argv[1]:
+            gui_download(download, theme=argv[1])
+    except IndexError:
+        print('Theme hasn\'t been specified. By-default theme will be applied')
+        gui_download(download)
 
 
 if __name__ == "__main__":
